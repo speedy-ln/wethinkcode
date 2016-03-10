f03fa6e28fc9a77b4732c9d76f19d34192662505
Lebelo Nkadimeng <mashesha@MacBook-Pro.local>
2016-03-11 01:22:56 +0200

    ex08

---
+++ day09/ex08/ft_spy.c
@@ -0,0 +1,16 @@
+int	main()
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (argv[argc])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == 'president' || 'attack' || Powers)
+	write(1, "Alert!!!", 8);
+		}
+	}
+}
