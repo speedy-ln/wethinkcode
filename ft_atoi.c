a9bdfea8af40c027f3a6c00dfeccda7d4b7989e9
Speedy <lnnkadimeng@gmail.com>
2016-05-04 14:36:51 +0200

    Create ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,28 @@
+int		ft_atoi(const char *str)
+{
+	int result;
+	int negatif;
+
+	result = 0;
+	negatif = 0;
+	if (str)
+	{
+		while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\r'
+			|| *str == '\f' || *str == '\v')
+			str++;
+		if (*str == '-')
+		{
+			negatif = 1;
+			str++;
+		}
+		else if (*str == '+')
+			str++;
+		while (*str >= '0' && *str <= '9')
+		{
+			result += *str++ - '0';
+			if (*str >= '0' && *str <= '9')
+				result *= 10;
+		}
+	}
+	return (negatif ? -result : result);
+}
