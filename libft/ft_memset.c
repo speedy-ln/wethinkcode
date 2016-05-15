5b195515b8978ba6eaaa40687f3c9016bb5a535f
Speedy <lnnkadimeng@gmail.com>
2016-05-15 11:31:33 +0200

    Create ft_memset.c

---
+++ libft/ft_memset.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t			cmp;
+	unsigned char	*ptr;
+
+	cmp = 0;
+	ptr = (unsigned char *)b;
+	while (cmp < len)
+	{
+		ptr[cmp] = (unsigned char)c;
+		cmp++;
+	}
+	return (b);
+}
