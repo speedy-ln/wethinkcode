933e96f6991ce814ca51d0fc0dc6b7f1066da1ae
Speedy <lnnkadimeng@gmail.com>
2016-05-15 12:27:50 +0200

    Create ft_memcmp.c

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*tmp;
+	unsigned char	*tmp2;
+
+	tmp = (unsigned char*)s1;
+	tmp2 = (unsigned char*)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (tmp[i] != tmp2[i])
+			return (tmp[i] - tmp2[i]);
+		else
+			i++;
+	}
+	return (0);
+}
