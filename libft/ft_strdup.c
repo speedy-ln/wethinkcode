a5d531910aa2415dd6354e05782bf5fd37e76fcc
Speedy <lnnkadimeng@gmail.com>
2016-05-15 10:24:03 +0200

    Create ft_strdup.c

---
+++ libft/ft_strdup.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	int	src_size;
+	char	*dest;
+
+	int i = 0;
+	int size_dest = 0;
+	while (src[src_size])
+		src_size++;
+	dest = (char*)malloc(sizeof(*dest) * (src_size));
+	while (src[i])
+	{
+		dest[i] = src[i]
+		i++;
+	}
+	dest[src_size] = '\0';
+	return (dest);
+}
