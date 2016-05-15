d6835d6703bf9555ce0ac3c75e0fdc9745d8d1b2
Speedy <lnnkadimeng@gmail.com>
2016-05-15 11:43:18 +0200

    Create ft_memchr.c

---
+++ libft/ft_memchr.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "libft.h"
+
+void	*ft_memchr(const void *s1, int c, size_t n)
+{
+	char		tmp;
+	char		*tmp2;
+
+	tmp = (unsigned char)c;
+	tmp2 = (char*)s1;
+	while (n--)
+	{
+		if (*tmp2 == tmp)
+			return (tmp2);
+		tmp2++;
+	}
+	return (NULL);
+}
